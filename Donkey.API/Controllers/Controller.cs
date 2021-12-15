a1981aeba6c7c1defe8a5d767d0e299f014e6a14
Kacper <kacper916@gmail.com>
2021-12-15 19:54:06 +0100

    Add project files.

---
+++ Donkey.API/Controllers/Controller.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Donkey.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Controller : ControllerBase
+    {
+     
+    }
+}
