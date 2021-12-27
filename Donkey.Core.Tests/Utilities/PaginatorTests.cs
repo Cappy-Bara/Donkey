e925413f30b29ae8a572f0b1a641db4cf26ea158
Kacper <kacper916@gmail.com>
2021-12-27 15:48:02 +0100

    :sparkles: pagination handler created

---
+++ Donkey.Core.Tests/Utilities/PaginatorTests.cs
@@ -0,0 +1,77 @@
+﻿using Donkey.Core.Utilities.Pagination;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Donkey.Tests.Core.Utilities
+{
+    public class PaginatorTests
+    {
+        
+        [Theory]
+        [InlineData(1,1,5)]
+        [InlineData(2,6,10)]
+        public void Paginator_ElementsDividedWithoutRest_ShouldReturnRightElements(int pageNumber, int firstElementIndex,int lastElementIndex)
+        {
+            List<int> values = new List<int>() {1,2,3,4,5,6,7,8,9,10};
+            
+            var paginator = new Paginator<int>(values,5);
+
+            var sus = paginator.GetElementsFromPage(pageNumber);
+
+            sus.LastElementIndex.Should().Be(lastElementIndex);
+            sus.FirstElementIndex.Should().Be(firstElementIndex);
+            sus.AvailablePages.Should().Be(2);
+            sus.Items.Count().Should().Be(5);
+        }
+
+        [Theory]
+        [InlineData(1, 1, 3, 3)]
+        [InlineData(2, 4, 6, 3)]
+        [InlineData(3, 7, 9, 3)]
+        [InlineData(4, 10, 10, 1)]
+        public void Paginator_ElementsDividedWithRest_ShouldReturnRightElementList(int pageNumber, int firstElementIndex, int lastElementIndex,int elementsOnPage)
+        {
+            List<int> values = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var paginator = new Paginator<int>(values, 3);
+
+            var sus = paginator.GetElementsFromPage(pageNumber);
+
+            sus.LastElementIndex.Should().Be(lastElementIndex);
+            sus.FirstElementIndex.Should().Be(firstElementIndex);
+            sus.AvailablePages.Should().Be(4);
+            sus.Items.Count().Should().Be(elementsOnPage);
+        }
+
+        [Fact]
+        public void Paginator_WantNotExistingPage_ShouldThrowException()
+        {
+            List<int> values = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var paginator = new Paginator<int>(values, 3);
+
+            var sus = paginator.GetElementsFromPage(8);
+
+            sus.Should().Be(PaginatedResult<int>.Empty);
+        }
+
+        [Theory]
+        [InlineData(1,"first","second","third")]
+        [InlineData(2,"fourth","fifth","sixth")]
+        public void Paginator_OrderedByValue_ShouldReturnRightElementList(int pageNumber, string firstElement,string secondElement = null,string thirdElement = null)
+        {
+            List<(string,int)> values = new List<(string, int)>() { ("fourth",4),("second",2),("fifth",5),("sixth",6),("seventh",7),("first",1),("third",3)};
+
+            var paginator = new Paginator<(string,int)>(values, 3);
+            paginator.OrderBy(x => x.Item2);
+            var sus = paginator.GetElementsFromPage(pageNumber);
+
+            sus.Items.Select(x => x.Item1).Should().ContainInOrder(firstElement,secondElement,thirdElement);
+        }
+    }
+}
---
+++ Donkey.Core/Utilities/Pagination/Paginator.cs
@@ -0,0 +1,52 @@
+﻿using Donkey.Core.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donkey.Core.Utilities.Pagination
+{
+    public class Paginator<T>
+    {
+        internal IEnumerable<T> _collection;
+        private readonly int _pageSize;
+
+        public Paginator(IEnumerable<T> collection, int pageSize)
+        {
+            _collection = collection;
+            _pageSize = pageSize;
+        }
+
+        public PaginatedResult<T> GetElementsFromPage(int pageNumber)
+        {
+            int pageCount = (int)Math.Ceiling(((double)_collection.Count() / (double)_pageSize));
+
+            if (pageNumber > pageCount)
+                return PaginatedResult<T>.Empty;
+
+            var items = _collection.Skip(_pageSize * (pageNumber - 1)).Take(_pageSize);
+            int firstElementIndex = (pageNumber - 1) * _pageSize + 1;
+            int lastElementIndex = firstElementIndex + items.Count() - 1;
+
+            var output = new PaginatedResult<T>()
+            {
+                Items = items,
+                AvailablePages = pageCount,
+                FirstElementIndex = firstElementIndex,
+                LastElementIndex = lastElementIndex
+            };
+
+            return output;
+        }
+    }
+
+    public static class PaginatorExtensions
+    {
+        public static Paginator<T> OrderBy<T,TKey>(this Paginator<T> paginator, Func<T, TKey> predicate)
+        {
+            paginator._collection = paginator._collection.OrderBy(predicate);
+            return paginator;
+        }
+    }
+}
