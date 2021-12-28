d40cde35593ec416d25681fc0c865418b8e2f2c2
Kacper <kacper916@gmail.com>
2021-12-28 17:44:28 +0100

    :lock: get posts query validation added.

---
+++ Donkey.API/Validation/PaginationValidators.cs
@@ -0,0 +1,20 @@
+﻿using Donkey.API.DTOs.Requests;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Donkey.API.Validation
+{
+    public class PaginationValidators : AbstractValidator<PaginationDto>
+    {
+        public PaginationValidators()
+        {
+            RuleFor(x => x.Page).GreaterThan(0).WithMessage("Page must be higher than 0");
+            RuleFor(x => x.Limit).GreaterThan(0).WithMessage("Limit must be higher than 0");
+        }
+        
+    }
+}
